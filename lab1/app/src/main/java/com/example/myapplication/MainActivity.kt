461e65d6d3b690393a9446c22587121a9494c2f6
Xiyao Xu <a710850142@gmail.com>
2024-09-02 19:21:01 -0600

    add

--- lab1/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ lab1/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,28 +3,29 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Toast
+import android.widget.Button
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button.setOnClickListener{
-            val intent = Intent(this, MainActivity2::class.java)
-            val selectedNum = binding.radioGroup.checkedRadioButtonId
-            val text = findViewById<RadioButton>(selectedNum).text
+        val buttons = listOf(
+            binding.button1,
+            binding.button2,
+            binding.button3,
+            binding.button4,
+            binding.button5
+        )
 
-            intent.putExtra("text", text.toString())
-            startActivity(intent)
+        buttons.forEach { button ->
+            button.setOnClickListener {
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("text", button.text.toString())
+                startActivity(intent)
+            }
         }
     }
 }
